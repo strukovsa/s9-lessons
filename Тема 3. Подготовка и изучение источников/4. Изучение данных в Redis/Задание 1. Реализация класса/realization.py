a19ef4f6194a788bf09218ca94fa57435f1d81d0
«strukovsa» <serrstr07@gmail.com>
2024-07-24 17:02:52 +0300

    Theme 3 completed

--- Тема 3. Подготовка и изучение источников/4. Изучение данных в Redis/Задание 1. Реализация класса/realization.py
+++ Тема 3. Подготовка и изучение источников/4. Изучение данных в Redis/Задание 1. Реализация класса/realization.py
@@ -3,12 +3,22 @@ from typing import Dict
 import redis
 
 
+
+
 class RedisClient:
     def __init__(self, host: str, port: int, password: str, cert_path: str) -> None:
-        self._client = redis.StrictRedis(...)# Write code here
-              
-		def set(self, k:str, v: Dict):
-				self._client ...# Write code here.
-
-    def get(self, k:str) -> Dict:
-				 self._client ...# Write code here.
+        self._client = redis.StrictRedis(host=f'{host}',
+    port=port,
+    password=password,
+    ssl=True,
+    ssl_ca_certs=cert_path,
+    )
+    def set(self, k: str, v: Dict[str, str]) -> None:
+        v_json = json.dumps(v)
+        self._client.set(k, v_json)
+    def get(self, k:str) -> str:
+        v = self._client.get(k)
+        if not v:
+            return None
+        j = json.loads(v)
+        return j
--- Тема 3. Подготовка и изучение источников/6. Изучение данных в Kafka/Задание 1. Реализация класса/realization.py
+++ Тема 3. Подготовка и изучение источников/6. Изучение данных в Kafka/Задание 1. Реализация класса/realization.py
@@ -1,7 +1,10 @@
 import json
 from typing import Dict, Optional
 from confluent_kafka import Consumer
+import time
 
+def error_callback(err):
+        print('Something went wrong: {}'.format(err))
 
 class KafkaConsumer:
     def __init__(self,
@@ -13,12 +16,35 @@ class KafkaConsumer:
                  group: str,
                  cert_path: str
                  ) -> None:
-        params = ...# напишите код здесь
+        params = {
+            'bootstrap.servers': f'{host}:{port}',
+            'security.protocol': 'SASL_SSL',
+            'ssl.ca.location': cert_path,
+            'sasl.mechanism': 'SCRAM-SHA-512',
+            'sasl.username': user,
+            'sasl.password': password,
+            'group.id': group,
+            'auto.offset.reset': 'earliest',
+            'enable.auto.commit': False,
+            'error_cb': error_callback,
+            'debug': 'all',
+            'client.id': 'someclientkey'
+        }
 
         self.consumer = Consumer(params)
         self.consumer.subscribe([topic])
 
     def consume(self, timeout: float = 3.0) -> Optional[Dict]:
-        msg = self.consumer.poll(timeout=timeout)
+        while True:
+            msg = self.consumer.poll(timeout=timeout)
 
-        ...# напишите код здесь
+            if msg is None:
+                time.sleep(1)
+                continue
+
+            if msg.error():
+                raise Exception(msg.error())
+        
+            val = msg.value().decode()
+            print(json.loads(val))
+            return json.loads(val)
